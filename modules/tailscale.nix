399910f3cedbdc2a210ba709028e9caa65250654
Callum <mail@callumwong.com>
2026-03-07 23:34:14 +1100

    [liz] remove tailscale auth key shenanigans

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
@@ -14,8 +13,6 @@ in
   config = lib.mkIf cfg.enable {
     services.tailscale.enable = true;
 
-    sops.secrets."tailscale/auth-key" = { };
-
     networking.firewall = {
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
@@ -24,35 +21,5 @@ in
     systemd.services.tailscaled.serviceConfig.Environment = [
       "TS_DEBUG_FIREWALL_MODE=nftables"
     ];
-
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      after = [
-        "network-pre.target"
-        "tailscaled.service"
-      ];
-      wants = [
-        "network-pre.target"
-        "tailscaled.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig.Type = "oneshot";
-
-      script = ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # check if we are already authenticated to tailscale
-        status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        ${pkgs.tailscale}/bin/tailscale up --auth-key=${config.sops.secrets."tailscale/auth-key".path}
-      '';
-    };
   };
 }
