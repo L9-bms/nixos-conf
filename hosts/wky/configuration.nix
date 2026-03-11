f8bb08ce9aa2046c8660d0c96b10bd906b4ebed1
callum <mail@callumwong.com>
2026-03-11 12:03:25 +1100

    [wky] attempt to fix bt audio

--- hosts/wky/configuration.nix
+++ hosts/wky/configuration.nix
@@ -55,6 +55,7 @@
   };
 
   hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
 
   nixpkgs.overlays = [
     inputs.niri.overlays.niri
@@ -76,9 +77,45 @@
 
   services.printing.enable = true;
 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
+    wireplumber.extraConfig."11-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+    };
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+      };
+    };
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      "context.properties" = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = { };
+        }
+      ];
+      "pulse.properties" = {
+        "pulse.min.req" = "32/48000";
+        "pulse.default.req" = "32/48000";
+        "pulse.max.req" = "32/48000";
+        "pulse.min.quantum" = "32/48000";
+        "pulse.max.quantum" = "32/48000";
+      };
+      "stream.properties" = {
+        "node.latency" = "32/48000";
+        "resample.quality" = 1;
+      };
+    };
   };
 
   programs.fish.enable = true;
