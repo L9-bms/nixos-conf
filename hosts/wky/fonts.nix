daa5d270517d32ce4c5ca202bd40f500e0aca235
callum <mail@callumwong.com>
2026-03-12 16:44:25 +1100

    [wky] add terminus

--- hosts/wky/fonts.nix
+++ hosts/wky/fonts.nix
@@ -10,18 +10,26 @@ in
     enableDefaultPackages = true;
     enableGhostscriptFonts = true;
     packages = with pkgs; [
+      # standard fonts
       noto-fonts
       cantarell-fonts
       liberation_ttf
       inter
+
+      # monospace fonts
       monaspace
       nerd-fonts.jetbrains-mono
       nerd-fonts.recursive-mono
       comic-mono-nf
       ioskeley-mono
+
+      # bitmap fonts
+      terminus_font
     ];
     fontconfig = {
       enable = true;
+      allowBitmaps = true;
+
       defaultFonts = {
         monospace = [ "Ioskeley Mono" ];
         sansSerif = [ "Inter" ];
