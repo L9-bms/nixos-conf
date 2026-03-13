bf1809024ebb66b2630ed71d117579a791757a5c
callum <mail@callumwong.com>
2026-03-13 23:57:17 +1100

    [wky] fix audio and replace ppd with tuned

--- hosts/wky/configuration.nix
+++ hosts/wky/configuration.nix
@@ -83,34 +83,17 @@
         "bluetooth.autoswitch-to-headset-profile" = false;
       };
     };
-    extraConfig.pipewire."92-low-latency" = {
+    extraConfig.pipewire = {
       "context.properties" = {
         "default.clock.rate" = 48000;
-        "default.clock.quantum" = 32;
-        "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
-      };
-    };
-    extraConfig.pipewire-pulse."92-low-latency" = {
-      "context.properties" = [
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = { };
-        }
-      ];
-      "pulse.properties" = {
-        "pulse.min.req" = "32/48000";
-        "pulse.default.req" = "32/48000";
-        "pulse.max.req" = "32/48000";
-        "pulse.min.quantum" = "32/48000";
-        "pulse.max.quantum" = "32/48000";
-      };
-      "stream.properties" = {
-        "node.latency" = "32/48000";
-        "resample.quality" = 1;
+        "default.clock.quantum" = 2048;
+        "default.clock.min-quantum" = 2048;
+        "default.clock.max-quantum" = 8192;
       };
     };
   };
+
+  services.auto-cpufreq.enable = true;
 
   programs.fish.enable = true;
   documentation.man.cache.enable = false; # prevent extra long build times
